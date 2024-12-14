9f4453f28b26f0c908c679f7bc510fb9f208e650
Tejiri <steveijatomi@gmail.com>
2024-12-14 21:12:09 +0000

    Add recipe page modified

--- app/src/main/java/com/xita/dailyhelper/pages/AddRecipePage.kt
+++ app/src/main/java/com/xita/dailyhelper/pages/AddRecipePage.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,6 +28,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -92,13 +96,24 @@ fun AddRecipePage(navController: NavHostController, innerPaddingValues: PaddingV
 //                    obscureText = false
 //                )
                 LazyColumn(Modifier.padding(horizontal = 10.dp)) {
-                    item {
+                    items(1) {
                         CustomFormComposables().CustomTextField(
                             onValueChange = {},
                             placeHolder = { Text("Recipe name") },
                             value = "",
                             obscureText = false
                         )
+
+                        OutlinedTextField(
+//                            visualTransformation = if (obscureText) PasswordVisualTransformation() else VisualTransformation.None,
+                            value = "",
+                            onValueChange = {  },
+                            shape = RoundedCornerShape(20),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 15.dp),
+minLines = 5
+                            )
 
                         CustomFormComposables().CustomButton(text = "Create Recipe", onClick = {})
                     }
